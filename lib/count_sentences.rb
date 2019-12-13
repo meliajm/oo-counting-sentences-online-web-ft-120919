cdc17a232a30c196a02c20909eee813d980c932b
melia miller <meliajm@gmail.com>
2019-12-13 02:51:32 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,10 +1,15 @@
  require 'pry'
 
 class String
+  
   # attr_accessor :s
   def sentence?(s)
     
     s[-1] == "." ? true : false
+    if s[-1] == "."
+      return true
+    else
+      return false
     
   end
   # binding.pry
