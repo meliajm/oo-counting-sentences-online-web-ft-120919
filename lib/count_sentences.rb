dd8137fd044c394d82cd845640c6744d40a71dea
melia miller <meliajm@gmail.com>
2019-12-13 02:59:13 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,13 +16,7 @@ class String
 
   def count_sentences
     count = 0
-    if sentence? 
-      count += 1
-    elsif question? 
-      count += 1
-    elsif exclamation?
-      count += 1
-    end
+    
 
   end
 end
